1300fe67dec68523df4bd1176413efe03f7bc562
rodrigocg123 <rocasgut@gmail.com>
2025-10-03 09:00:24 +0200

    ejercicio5 hecho

--- src/ejerciciosClase/Ejercicio5.java
+++ src/ejerciciosClase/Ejercicio5.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Ejercicio5 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         
+        // Declaración de las dos variables principales
+        System.out.println("Introduce el primer número: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.println("Introduce el segundo número: ");
+        double num2 = scanner.nextDouble();
+        
+        // Variables auxiliares para cada operación
+        double auxSuma = num1 + num2;
+        double auxResta = num1 - num2;
+        double auxMultiplicacion = num1 * num2;
+        double auxDivision = num1 / num2;
+        
+        // Mostrar resultados
+        System.out.println("La suma es: " + auxSuma);
+        System.out.println("La resta es: " + auxResta);
+        System.out.println("La multiplicación es: " + auxMultiplicacion);
+        System.out.println("La división es: " + auxDivision);
+        
+        scanner.close();
     }
-}
+}        
+    
