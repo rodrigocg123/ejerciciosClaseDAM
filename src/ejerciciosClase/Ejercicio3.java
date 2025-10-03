57a6a1e719f9ec6da156cb5f386d0f110c154686
rodrigocg123 <rocasgut@gmail.com>
2025-10-03 08:43:58 +0200

    Ejercicio3 hecho

--- src/ejerciciosClase/Ejercicio3.java
+++ src/ejerciciosClase/Ejercicio3.java
@@ -1,3 +1,10 @@
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        int N = 5;
+        int resultado = N + 77;
+        int resultado2 = resultado - 3;
+        System.out.println("primero vamos a incrementar a " + N + " + 77 "+ " valor el cual es = " + resultado + "\n");
+        System.out.println("luego decrementamos a " + resultado + " - 3 "+ " valor el cual es = " + (resultado - 3)+ "\n");
+        System.out.println("luego multiplicamos a " + resultado2 + " * 2 "+ " valor el cual es = " + (resultado2 * 2));
+    }
 }
